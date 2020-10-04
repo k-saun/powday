5d2070ae797e159f43a85f81343e15be2d3af2db
OnionTurbo <ericpeterson175@u.boisestate.edu>
2020-10-04 16:23:53 -0600

    Added full npm path

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 cd /home/ec2-user/live_files;
 cd react-front;
-npm install react;
-npm install react-dom;
-npm run build;
+/root/.nvm/versions/node/v14.13.0/bin/npm install react;
+/root/.nvm/versions/node/v14.13.0/bin/npm install react-dom;
+/root/.nvm/versions/node/v14.13.0/bin/npm run build;
 cd ..;
 cd flask-back;
 nohup python3 app.py &
+exit 0;
