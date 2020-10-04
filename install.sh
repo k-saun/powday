80b88f559bf32a4ba39b121c222ef835d8644643
OnionTurbo <ericpeterson175@u.boisestate.edu>
2020-10-04 17:45:47 -0600

    Working with Ubuntu now

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-cd /home/ec2-user/live_files;
+cd /home/ubuntu/live_files;
 cd react-front;
-npm config set registry http://registry.npmjs.org/;
 npm install react;
 npm install react-dom;
 npm install run build;
