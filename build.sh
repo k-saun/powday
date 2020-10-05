5d55ff8f798be87d4aac0266e0cb47d891389608
OnionTurbo <ericpeterson175@u.boisestate.edu>
2020-10-04 20:49:48 -0600

    I think the issue may be that NPM is being run as root

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/ec2-user/live_files/react-front;
+npm config set registry http://registry.npmjs.org/;
+npm install react;
+npm install react-dom;
+npm install run build;
--- install.sh
+++ install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-cd /home/ec2-user/live_files/react-front;
-npm config set registry http://registry.npmjs.org/;
-npm install react;
-npm install react-dom;
-npm install run build;
 cd /home/ec2-user/live_files/flask-back;
 python3 /home/ec2-user/live_files/flask-back/app.py &
 exit 0;
