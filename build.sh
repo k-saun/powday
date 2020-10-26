54fa90647b147cd0b51c46eb73ab6a781dc71691
OnionTurbo <ericpeterson175@u.boisestate.edu>
2020-10-25 20:33:23 -0600

    Removed uneeded files for AWS

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/ubuntu/live_files/react-front;
-#npm config set registry http://registry.npmjs.org/;
-npm install react;
-npm install react-dom;
-npm install react-map-gl;
-npm run build;
